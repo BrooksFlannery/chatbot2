62eb5c8b916ef22f37fd42dd9131fe1fe7bf48be
JosephBFlannery <brooks.flannery2000@gmail.com>
2025-08-30 23:17:22 -0400

    added flowglad

---
+++ src/flowglad.ts
@@ -0,0 +1,9 @@
+// flowglad.ts
+import { FlowgladServer } from '@flowglad/nextjs/server'
+import { currentUser } from '@clerk/nextjs/server'
+
+export const flowgladServer = new FlowgladServer({
+  clerk: {
+    currentUser,
+  },
+})
